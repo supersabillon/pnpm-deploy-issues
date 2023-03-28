7c6c6f9f56047d5bd25344435d267ca9fb22be2d
Joel Sabillon <joel.sabillon@disneystreaming.com>
2023-03-27 19:35:26 -0700

    reproducing issue with pnpm deploy

---
+++ apps/docs/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "ui";
+
+export default function Docs() {
+  const a: string = 'yo';
+  return (
+    <div>
+      <h1>Docs</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ packages/ui/Button.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+export const Button = () => {
+  const a: string = 'hehehehehehhehehehehdfdsafsfdsfds111111111';
+  return <button>Boosfsdfdsfdsf33333!!#####adfadsfdsfds</button>;
+};
